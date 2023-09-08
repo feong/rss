cc490c7862cee34a3888658945e04aaefc065244
Xiaofeng Liu <feong@live.com>
2023-09-09 00:49:49 +0800

    fix: disable mobile touch

--- src/index.tsx
+++ src/index.tsx
@@ -22,8 +22,6 @@ root.render(
   </React.StrictMode>,
 );
 
-document.body.addEventListener('touchmove', function(event) { event.preventDefault(); }, { passive: false });
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
